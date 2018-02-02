1802bb6095bc6b427e6e1ba403e5c3c802b91861
Manthan2211 <manthanandharia@gmail.com>
2018-02-02 14:43:11 +0530

    upto password reset

--- static_page/config/routes.rb
+++ static_page/config/routes.rb
@@ -1,8 +1,25 @@
 Rails.application.routes.draw do
+  
+
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
+  get 'sessions/new'
+
   root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get  'static_pages/about'
+  get 'static_pages/contact'
+  get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  resources :users
+  resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
